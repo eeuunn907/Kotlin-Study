9aae055acb04f69b681dfbc21f04694c5ae2e8d8
Yoohaeun <haspring@dsm.hs.kr>
2023-07-24 15:44:35 +0900

    lec01

---
+++ src/main/kotlin/school/lec01/Person.java
@@ -0,0 +1,15 @@
+package school.lec01;
+
+public class Person {
+
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    // Getter 라고 할 수 있음
+    public String getName() {
+        return name;
+    }
+}
